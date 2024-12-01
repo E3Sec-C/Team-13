755a4349ec3b5d0b4543259b63a8d3c203bed94b
TharunYetti <tharunyetti24@gmail.com>
2024-12-01 23:25:08 +0530

    Add snackbars for warnings while protecting

--- frontend/src/ProtectedRoute.js
+++ frontend/src/ProtectedRoute.js
@@ -1,8 +1,13 @@
 import React,{useEffect, useState} from "react";
 import { Navigate } from "react-router-dom";
 
+import { setSnackBar } from "./store/features/snackbar/snackbar";
+import { useDispatch } from "react-redux";
 
 const ProtectedRoute = ({ children, access }) => {
+
+    const dispatch = useDispatch();
+
     const [role, setRole] = useState(localStorage.getItem("role"));
     useEffect(() => {
         const interval = setInterval(() => {
@@ -14,10 +19,23 @@ const ProtectedRoute = ({ children, access }) => {
         return () => clearInterval(interval); // Cleanup on unmount
       }, [role]);
     if (!role) {
+        dispatch(
+          setSnackBar({
+            message: "Unauthorized access denied",
+            variant: "error",
+          })
+        )
         return <Navigate to="/" />;
       }
     
-      return role === access ? children : <Navigate to="/signin" />;
+      return role === access ? children : (
+        dispatch(
+          setSnackBar({
+            message: "Unauthorized access denied",
+            variant: "error",
+          })
+        ) && 
+      <Navigate to="/signin" />);
 };
 
 export default ProtectedRoute;
